3af44df279983af73a14337b57364bf1396370a8
gheysiell <gheysiellfarukzinn@hotmail.com>
2022-08-14 15:45:55 -0400

    add README.md and refatoration of code

---
+++ README.md
@@ -0,0 +1,5 @@
+# update_tecnospeed_events
+
+<div> <h2> Script feito em nodeJs para consultar API e atualizar campos em banco de dados </h2> </div>
+<div> <h5> ° Crie o arquivo .env e configure-o de acordo com seu ambiente </h5> </div>
+<div> <h5> ° Para instalar dependências, npm install </h5> </div>
--- src/app.js
+++ src/app.js
@@ -4,39 +4,23 @@ const db = require('./db')
 
 const events_updating = async () => {
     const conn = await db.connect()
-
     if (conn) {
-        console.log(conn)
+        const [rows] = await connection.execute(`SELECT id_webhook FROM ${configs.table}`)
 
-        //const [rows] = await conn.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?')
-        // rows.map(row => {
-        //     console.log(row)
-        // })
-
-        /*const query_all = await model_events_tecnospeed.findAll()
-        const events = query_all.map((event) => {
-            return event.dataValues.id_webhook
-        })*/
-
-        /*events.map(async event => {
-            await axios.get(`https://pix.tecnospeed.com.br/api/v1/pix/${event}`, {
+        rows.map(async row => {
+            await axios.get(`https://pix.tecnospeed.com.br/api/v1/pix/${row.id_webhook}`, {
                 headers: {
                     "Authorization": configs.token_api
                 }
             })
-            .then(response => {
-                console.log(response.data.amount)
-                console.log(conn)
-                model_events_tecnospeed.update({valor: response.data.amount}, {
-                    where: {
-                        id_webhook: event
-                    }
-                })
+            .then(async response => {
+                console.log(`${response.data.id} - ${response.data.amount}`)
+                await connection.execute(`UPDATE ${configs.table} SET valor = ${response.data.amount} WHERE id_webhook = '${row.id_webhook}'`)
             })
             .catch(err => {
                 console.log(`erro: ${err}`)
             })
-        })*/
+        })
     }
 }
 
